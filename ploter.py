7de71338805e1c6926c1e7db186fcdf76b9de8cf
esasharahi <esasharahi@gmail.com>
2018-09-10 00:08:10 +0430

    Update ploter.py

--- ploter.py
+++ ploter.py
@@ -1,47 +1,32 @@
 ## esasharahi@gmail.com
 #!/usr/bin/python
-from mpl_toolkits.mplot3d import Axes3D
+import numpy as np  
 import matplotlib.pyplot as plt
-
+from mpl_toolkits.mplot3d import Axes3D
 
 fig2d = plt.figure()
 fig3d = plt.figure()
 
+data1 = np.loadtxt('points1.txt')
+data2 = np.loadtxt('points2.txt')
 
-with open('points1.txt') as f_1:
-	lines = f_1.readlines()
-	x1 = [line.split()[0] for line in lines]
-	y1 = [line.split()[1] for line in lines]
-
-with open('points2.txt') as f_2:
-	lines = f_2.readlines()
-	x2 = [line.split()[0] for line in lines]
-	y2 = [line.split()[1] for line in lines]
-
-with open('points3d.txt') as f3d:
-	lines = f3d.readlines()
-	t = [float(line.split()[0]) for line in lines]
-	x = [float(line.split()[1]) for line in lines]
-	y = [float(line.split()[2]) for line in lines]
-
+data3 = np.loadtxt('points3d.txt')
 
 ax = fig2d.add_subplot(1, 1, 1)
-ax.plot(x1, y1, c = 'red', label = '$y_1$')
-ax.plot(x2, y2, c = 'blue', label = '$y_2$')
+ax.plot(data1[:,0], data1[:,1], c = 'red', label = '$y_1$')
+ax.plot(data2[:,0], data2[:,1], c = 'blue', label = '$y_2$')
 leg = ax.legend()
 ax.set_title("Runge-Kutta4 Method for the $\mathrm{d}y_i / \mathrm{d}t = f_i(t, y_1, y_2)$")
 ax.set_xlabel('$t$')
 ax.set_ylabel('$y_i$')
 
-
 ax3d = plt.axes(projection='3d')
-ax3d.plot(t, x, y, c = 'violet', label = '$(t, y_1, y_2)$')
+ax3d.plot(data3[:,0], data3[:,1], data3[:,2], c = 'violet', label = '$(t, y_1, y_2)$')
 leg = ax3d.legend(bbox_to_anchor=(0.1, 0))
 ax3d.set_title("Curve derived from the Runge-Kutta4 Method for the $\mathrm{d}y_i / \mathrm{d}t = f_i(t, y_1, y_2)$")
 ax3d.set_xlabel('$t$')
 ax3d.set_ylabel('$y_1$')
 ax3d.set_zlabel('$y_2$')
-
 
 fig2d.savefig('Runge-Kutta_Result.eps', format='eps', dpi=1000)
 fig3d.savefig('Runge-Kutta_Result_3d.eps', format='eps', dpi=1000)
